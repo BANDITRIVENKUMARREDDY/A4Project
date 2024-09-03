014dd28132ac417dd36757446c4c5784677eb8ff
trive <trive@Nani>
2024-09-03 14:24:29 +0530

    hi reddy

---
+++ org.Ecomerce.Sample/src/main/java/generic/BANDITRIVENKUMARDSAMPLE.java
@@ -0,0 +1,14 @@
+package generic;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+
+public class BANDITRIVENKUMARDSAMPLE {
+
+	public static void main(String[] args) {
+		System.out.println("Bandi triven kumar reddy");
+			
+		}
+
+	}
+
+
